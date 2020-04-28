c4205d0957daea499b2574d917bc329a6b8b3bdd
DaVv <davv09@gmail.com>
2020-04-28 14:59:42 +0200

    MVC : Afficher le descriptif de l'oeuvre avec Servlet et JSP

--- src/main/java/com/directmedia/onlinestore/frontoffice/controller/WorkDetailsServlet.java
+++ src/main/java/com/directmedia/onlinestore/frontoffice/controller/WorkDetailsServlet.java
@@ -3,6 +3,7 @@ package com.directmedia.onlinestore.frontoffice.controller;
 import com.directmedia.onlinestore.core.entity.Catalogue;
 import com.directmedia.onlinestore.core.entity.Work;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,33 +17,17 @@ import java.io.PrintWriter;
 public class WorkDetailsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        String sId= req.getParameter("id");
+        String sId = req.getParameter("id");
 
-        Work oeuvre=null;
-        for(Work newWork: Catalogue.listOfWorks){
-           if(newWork.getId()==Long.parseLong(sId)){
-             oeuvre=newWork;
-           }
+        Work selectedWork = null;
+        for (Work work : Catalogue.listOfWorks) {
+            if (work.getId() == Long.parseLong(sId)) {
+                selectedWork = work;
+                req.setAttribute("selectedWork", selectedWork);
+                RequestDispatcher disp = req.getRequestDispatcher("/work-details.jsp");
+                disp.forward(req, resp);
+            }
         }
-
-        resp.setContentType("text/html;charset=UTF-8");
-        out.print("<html>");
-        out.print(" <head>");
-        out.print("<title>Details de l'oeuvre</title>");
-        out.print("</head>");
-        out.print("<body>");
-        out.print("Titre : "+ oeuvre.getTitle()+"<BR/>");
-        out.print("Année : "+ oeuvre.getRelease()+"<BR/>");
-        out.print("Genre : "+ oeuvre.getGenre()+"<BR/>");
-        out.print("Artiste: "+ oeuvre.getMainArtist().getName()+"<BR/>");
-        out.print("Résumé : "+ oeuvre.getSummary()+"<BR/>");
-        out.print("<form action=\"addToCart\" method=\"post\">\n" +
-                "    <input type=\"hidden\" name=\"idSelectionAchat\" value="+oeuvre.getId()+"><br/>\n" +
-                "    <input type=\"submit\" value=\"ajouter au panier\">\n" +
-                "</form>");
-        out.print("</body>");
-        out.print("</html>");
     }
 }
 
